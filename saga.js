ca973d0cd5ea38c0d609c19742097d17bbeb6273
anchaljain123 <anchaljain14@gmail.com>
2019-02-06 15:47:58 +0530

    cancel effect

--- saga.js
+++ saga.js
@@ -1,4 +1,4 @@
-import { put, takeEvery, all, call, takeLatest, take, select, fork } from 'redux-saga/effects'
+import { put, takeEvery, all, call, takeLatest, take, select, fork, cancel, cancelled } from 'redux-saga/effects'
 import Api from './api';
 
 
@@ -9,25 +9,25 @@ function* watchAndLog() {
     while (true) {
         const action = yield take('*')
         const state = yield select()
-    
+
         console.log('action', action)
         console.log('state after', state)
-      }
-  }
+    }
+}
 
 function fetchProductsApi() {
     return Api.fetch('/products')
-      .then(response => ({ response }))
-      .catch(error => ({ error }))
-  }
-  
-  function* fetchProducts() {
+        .then(response => ({ response }))
+        .catch(error => ({ error }))
+}
+
+function* fetchProducts() {
     const { response, error } = yield call(fetchProductsApi)
     if (response)
-      yield put({ type: 'PRODUCTS_RECEIVED', products: response })
+        yield put({ type: 'PRODUCTS_RECEIVED', products: response })
     else
-      yield put({ type: 'PRODUCTS_REQUEST_FAILED', error })
-  }
+        yield put({ type: 'PRODUCTS_REQUEST_FAILED', error })
+}
 
 export function* helloSaga() {
     console.log('Hello Sagas!')
@@ -37,7 +37,7 @@ export function* helloSaga() {
 export function* fetchData(action) {
     try {
         //yield Api.fetch('/products')
-       const data = yield call(Api.fetchUser, action.payload.url) //'call' use for testing
+        const data = yield call(Api.fetchUser, action.payload.url) //'call' use for testing
         /*
        {
             CALL: {
@@ -46,54 +46,68 @@ export function* fetchData(action) {
             }
         }     
         */
-       //-----------------dispatching actions-------------------
+        //-----------------dispatching actions-------------------
 
-       // dispatch({ type: 'FETCH_SUCCEEDED', data })
-       yield put({type: "FETCH_SUCCEEDED", data}) //for testing use 'put'
+        // dispatch({ type: 'FETCH_SUCCEEDED', data })
+        yield put({ type: "FETCH_SUCCEEDED", data }) //for testing use 'put'
 
     } catch (error) {
-       yield put({type: "FETCH_FAILED", error})
+        yield put({ type: "FETCH_FAILED", error })
     }
- }
+}
 
 // Our worker Saga: will perform the async increment task
 export function* incrementAsync() {
     yield call(delay, 1000)
- 
+
     yield put({ type: 'INCREMENT' })
 }
-  
-  // Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
+
+// Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
 export function* watchIncrementAsync() {
     yield takeEvery('INCREMENT_ASYNC', incrementAsync)
 }
 
 
-function* authorize(user, password) {
+export function* authorize(user, password) {
     try {
-      const token = yield call(Api.authorize, user, password)
-      yield put({type: 'LOGIN_SUCCESS', token})
-      return token
-    } catch(error) {
-      yield put({type: 'LOGIN_ERROR', error})
+        const token = yield call(Api.authorize, user, password)
+        yield put({ type: 'LOGIN_SUCCESS', token })
+        return token
+    } catch (error) {
+        yield put({ type: 'LOGIN_ERROR', error })
+    } finally {
+        if (yield cancelled()) {
+            // ... put special cancellation handling code here
+        }
     }
-  }
+}
 
-  function* loginFlow() {
-    while (true) {
-        const {user, password} = yield take('LOGIN_REQUEST')
-        yield fork(authorize, user, password)
-        yield take(['LOGOUT', 'LOGIN_ERROR']) //now using fork we can do logout before user is authorised 
-        yield call(Api.clearItem, 'token')
-      }
-  }
+export function* loginFlow() {
+        while (true) {
+            const { user, password } = yield take('LOGIN_REQUEST')
+            yield fork(authorize, user, password)
+            yield take(['LOGOUT', 'LOGIN_ERROR']) //now using fork we can do logout before user is authorised 
+            if (action.type === 'LOGOUT')
+                yield cancel(task) //If the task is still running, it'll be aborted.
+                // If the task has already completed then nothing will happen and the 
+                //cancellation will result in a no-op. 
+            yield call(Api.clearItem, 'token')
+        }
+    }
 
+//llly, like promise.all ----------
+    const [users, repos] = yield all([
+        call(fetch, '/users'),
+        call(fetch, '/repos')
+      ])
 
+//------------rootsaga--------
 export default function* rootSaga() {
-    yield all([
-      helloSaga(),
-      watchAndLog(),
-      watchIncrementAsync(),
-     yield takeLatest('FETCH_REQUESTED', fetchData)
-    ])
-  }
+        yield all([
+            helloSaga(),
+            watchAndLog(),
+            watchIncrementAsync(),
+            yield takeLatest('FETCH_REQUESTED', fetchData)
+        ])
+    }
